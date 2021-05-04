57d565278f8a595600da9240a2faf3b9d4826fd1
Ilya Kochankov <ilyakochankov@yandex.ru>
2021-05-04 14:09:30 +0300

    two spaces

--- modules/task_4/kochankov_i_shell_sort_simple_std/shell_sort_simple_std.h
+++ modules/task_4/kochankov_i_shell_sort_simple_std/shell_sort_simple_std.h
@@ -11,4 +11,4 @@ std::vector<double> shell_sort_std(const std::vector<double> &vec,
 std::vector<double> merge(const std::vector<double> &a,
                           const std::vector<double> &b);
 
-#endif // MODULES_TASK_4_KOCHANKOV_I_SHELL_SORT_SIMPLE_STD_SHELL_SORT_SIMPLE_STD_H_
+#endif  // MODULES_TASK_4_KOCHANKOV_I_SHELL_SORT_SIMPLE_STD_SHELL_SORT_SIMPLE_STD_H_
