28f654e1e684745dddc842b052de822ec541a248
Restyled.io <commits@restyled.io>
2021-05-04 09:52:30 +0000

    Restyled by clang-format

--- modules/task_4/kochankov_i_shell_sort_simple_std/shell_sort_simple_std.h
+++ modules/task_4/kochankov_i_shell_sort_simple_std/shell_sort_simple_std.h
@@ -4,11 +4,11 @@
 
 #include <vector>
 
-
 std::vector<double> getRandomVector(int sz);
-std::vector<double> shell_sort(const std::vector<double>& vec);
-std::vector<double> shell_sort_std(const std::vector<double>& vec, int num_threads = 4);
-std::vector<double> merge(const std::vector<double>& a, const std::vector<double>& b);
+std::vector<double> shell_sort(const std::vector<double> &vec);
+std::vector<double> shell_sort_std(const std::vector<double> &vec,
+                                   int num_threads = 4);
+std::vector<double> merge(const std::vector<double> &a,
+                          const std::vector<double> &b);
 
-
-#endif  // MODULES_TASK_4_KOCHANKOV_I_SHELL_SORT_SIMPLE_STD_SHELL_SORT_SIMPLE_STD_H_
+#endif // MODULES_TASK_4_KOCHANKOV_I_SHELL_SORT_SIMPLE_STD_SHELL_SORT_SIMPLE_STD_H_
