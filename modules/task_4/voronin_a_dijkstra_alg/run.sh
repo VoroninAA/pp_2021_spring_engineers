07363d01852bff2f9cddd05a2f5760b2d5555a7d
Voronin Aleksey <voralexan@gmail.com>
2021-05-26 21:11:34 +0300

    test

---
+++ modules/task_4/voronin_a_dijkstra_alg/run.sh
@@ -0,0 +1,8 @@
+
+NAME=$(pwd|tr / "\n"|tail |tail -1)_std
+cd ../../../build
+cmake -D USE_SEQ=ON -D USE_OMP=ON -D USE_TBB=ON -D USE_STD=ON -D USE_LATEX=ON ..
+cmake --build . --config RELEASE
+cd bin/
+ls -la
+./$NAME
